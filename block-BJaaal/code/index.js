96c5dcdb4ebe38624ae29b55e87a5e18f606937a
Biplab Bakchi <biplabbakchi3@gmail.com>
2022-05-23 19:41:55 +0530

    Final push

--- block-BJaaal/code/index.js
+++ block-BJaaal/code/index.js
@@ -15,17 +15,19 @@ let lastName = "Stark";
   - Alert message saying `I am [username] and my bio is [bio]`. Replace square brackets with the value of variable.
   - Alert the message `I'm Arya Stark of Winterfell` using the variable name firstName and lastName.
 */
-"I" + " " + "am" + " " + username + " " + "and"  + " " + "my" + " " + "bio" + " " + "is"  + " " + bio;
+alert ("I am" + " " + username + " " + "and my bio is" + " " + bio);
 
-"I" + " " + "am" + " " + firstName+ " " + lastName  + " " + "of" + " " + "Winterfell";
+
+
+alert ("I\'m" + " " + firstName + " " + lastName + " " + "of Winterfell");
 
 /* Do the following using string template literal
   - Alert message saying `I am [username] and my bio is [bio]`. Replace square brackets with the value of variable.
   - Alert the message `I'm Arya Stark of Winterfell` using the variable name firstName and lastName.
 */
-` I am ${username} and my bio is ${bio}`
-` I am ${firstName} ${lastName}  of Winterfell`
-
+ alert (` I am ${username} and my bio is ${bio}`)
+ alert (` I \'m ${firstName} ${lastName}  of Winterfell`)
+ 
 let amount = 200;
 const taxPercent = 10;
 
@@ -36,8 +38,8 @@ const taxPercent = 10;
   - Above message should be in two lines
 */
 
-`The total amount is ${amount} 
-the tax amount is ${amount} * ${taxPercent} / 100`
+alert (`The total amount is ${amount}
+the tax amount is ${(amount * taxPercent) / 100}`);
 
 
 /* Use double quote and escape character:
@@ -46,4 +48,5 @@ the tax amount is ${amount} * ${taxPercent} / 100`
   `
   - Above message should be in two lines
 */
+alert ("Thhe total amount is" + " " + amount + " " + "\n the tax amount is" + " " + ((amount * taxPercent) / 100) );
 
