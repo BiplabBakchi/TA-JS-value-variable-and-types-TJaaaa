aa86d61f827f767565fa64dbcd2ba4210f67a9b9
Biplab Bakchi <biplabbakchi3@gmail.com>
2022-05-23 18:51:43 +0530

    Initial push

--- block-BJaaal/code/index.js
+++ block-BJaaal/code/index.js
@@ -1,9 +1,12 @@
 //  Template literal
 
 /* Do the following:
-- Define a variable named `username` using `const` keyword and assign your name as a value.
+- Define a variable named `username` using `const` keyword and assign your name as a value. 
+
 - Define another variable named `bio` and assign a value `Learning to code`. Use `let` keyword to define the variable.
 */
+const username = "Biplab";
+let bio = "learning to code";
 
 let firstName = "Arya";
 let lastName = "Stark";
@@ -12,11 +15,16 @@ let lastName = "Stark";
   - Alert message saying `I am [username] and my bio is [bio]`. Replace square brackets with the value of variable.
   - Alert the message `I'm Arya Stark of Winterfell` using the variable name firstName and lastName.
 */
+"I" + " " + "am" + " " + username + " " + "and"  + " " + "my" + " " + "bio" + " " + "is"  + " " + bio;
+
+"I" + " " + "am" + " " + firstName+ " " + lastName  + " " + "of" + " " + "Winterfell";
 
 /* Do the following using string template literal
   - Alert message saying `I am [username] and my bio is [bio]`. Replace square brackets with the value of variable.
   - Alert the message `I'm Arya Stark of Winterfell` using the variable name firstName and lastName.
 */
+` I am ${username} and my bio is ${bio}`
+` I am ${firstName} ${lastName}  of Winterfell`
 
 let amount = 200;
 const taxPercent = 10;
@@ -28,9 +36,14 @@ const taxPercent = 10;
   - Above message should be in two lines
 */
 
+`The total amount is ${amount} 
+the tax amount is ${amount} * ${taxPercent} / 100`
+
+
 /* Use double quote and escape character:
   - Alert a message saying `The total amount is [amount]
     the tax amount is [(amount * taxPercent) / 100]
   `
   - Above message should be in two lines
 */
+
