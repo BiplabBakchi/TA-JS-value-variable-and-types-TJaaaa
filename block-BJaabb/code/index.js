30645535b807577048483f54bac0c14334e4b780
Biplab Bakchi <biplabbakchi3@gmail.com>
2022-05-25 21:55:13 +0530

    initial push

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -6,25 +6,54 @@
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
 
+{let user = prompt (`What is your name?`);
+  alert (`${user}`);
+}
+
+let userName = user;
+  console .log (`${user}, ${userName}`);
+let age = `22`; isLearning = `true`;
+
+{let user = `John`;
+  console .log (`${user}, ${userName}`);
+}
+
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
+let numA = Number (prompt (`Write the first number?`));
+let numB = Number (prompt (`Write the second number?`));
+let sum = (`${numA + numB}`);
+  alert (`${sum}`);
 
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
 
-for (let j = 0; j <= 10; ____) console.log(j);
+for (let j = 0; j <= 10; j++) {
+    if (j % 2 === 0) {
+        console.log(j)
+    } 
+}
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
-
+for (let j = 1; j <= 9; j++) {
+    if (j % 2 !== 0) {
+        console.log(j)
+    } 
+}
 // 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
-
+for (let j = 0; j <= 5; j++) {
+    console.log(j);
+}
 // 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
 
 // Comparisoin
 
 /* 7. Take two value using prompt and store them in variables `num1` and `num2`. Check whether they are equal or not.
+
+
+
 
 - `alert` true or false base on the entered value.
 - if the input value is anything like `true`, `null` or `undefined` alert saying `Enter a valid value`.
@@ -36,3 +65,14 @@ Example:
 "hello", -21; // false
 
 */
+let num1 = (prompt (`Write the first input?`));
+let num2 = (prompt (`Write the second input?`));
+
+if (num1 == num2) {
+    alert(`True`);
+} else if (num1 != num2) {
+    alert(`false`)
+} else {
+    alert(`Enter a valid value`)
+}
+
