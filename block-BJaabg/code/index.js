dec4e38a9ef9f48295923ec2ef9b3e36ba7d546d
Biplab Bakchi <biplabbakchi3@gmail.com>
2022-05-26 18:59:15 +0530

    Done

--- block-BJaabg/code/index.js
+++ block-BJaabg/code/index.js
@@ -13,45 +13,17 @@ const ACCESSORY_PRICE = 9.99;
 
 var bank_balance = 303.91;
 var amount = 0;
-
-function calculateTax(amount) {
-    return amount * TAX_RATE;
-}
-
-function formatAmount(amount) {
-    return "$" + amount.toFixed( 2 );
-}
-
-
+//your code goes here
 while (amount < bank_balance) {
-   
     amount = amount + PHONE_PRICE;
-
-    
     if (amount < SPENDING_THRESHOLD) {
         amount = amount + ACCESSORY_PRICE;
     }
 }
+let tax = amount * TAX_RATE;
+console.log(`$${amount + tax}`)
 
-
-amount = amount + calculateTax( amount );
-
-console.log(
-    "Your purchase: " + formatAmount( amount )
-);
-
-
-
-if (amount > bank_balance) {
-    console.log(
-        "You can't afford this purchase."
-    );
-}
-
-
-//your code goes here
-
-
+// Answer of the above `$334.76`.
 
 
 
